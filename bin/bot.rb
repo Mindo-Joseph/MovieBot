0878032ab915a36b41bbaefb7cd8adbb4d454d9d
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-05-12 18:50:12 +0300

    feat: make bot.rb executable and prompt user for movie genre

---
+++ bin/bot.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'telegram/bot'
+require './token.rb'
+token = $token
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Which films are u interested In?")
+      
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    
+    when '/action'
+      bot.api.send_message(chat_id: message.chat.id, text: "Action films ")
+    end
+
+  end
+end
