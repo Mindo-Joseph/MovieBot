af9713152e0993dd6b0a6e298391bdd2a69541a5
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-05-12 19:11:35 +0300

    feat: user chooses the command corresponding to their preffered genre

--- bin/bot.rb
+++ bin/bot.rb
@@ -9,13 +9,19 @@ Telegram::Bot::Client.run(token) do |bot|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      bot.api.send_message(chat_id: message.chat.id, text: "Which films are u interested In?")
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome, I am here to give you movie recommendations")
+      bot.api.send_message(chat_id: message.chat.id, text: "
+        Type on one of the following commands for the genre you want \n /action for action movies \n /comedy for comedy \n /romance for romance \n or /stop to end this session  ")
+         
+         
+       
+       
       
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}, hope to hear from you soon ")
     
     when '/action'
-      bot.api.send_message(chat_id: message.chat.id, text: "Action films ")
+      bot.api.send_message(chat_id: message.chat.id, text: "Great choice! Lemme get what might interest you")
     end
 
   end
