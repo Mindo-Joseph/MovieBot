2f58769aede82bda6c4a8d3030137d9670f4abb0
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-05-12 18:36:53 +0300

    feat(setup) : remove api key from bot.rb

--- bot.rb
+++ bot.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 require 'telegram/bot'
-token = '1071576461:AAH4gh_4fpKgaeTWt9dZE2PEzwQNif9p-KA'
+require './token.rb'
+token = $token
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Which films are u interested In?")
+      
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    
+    when '/action'
+      bot.api.send_message(chat_id: message.chat.id, text: "Action films ")
     end
+
   end
 end
